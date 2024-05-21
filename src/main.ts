b04a6d12d6e24346c74a03abe53acf135355fcd6
Safdar <safdar.hussain@devorbis.com>
2024-05-21 11:59:30 +0500

    add nest related vercel file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { configDotenv } from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors()
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
+    whitelist: true,
   }))
   await app.listen(3333);
 }
