801c9d82c482d31d3240fe7d1e79847bb0ec6aa9
Safdar <safdar.hussain@devorbis.com>
2024-05-24 01:37:13 +0500

    add alias package

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'module-alias/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }))
+       whitelist: true,
+  })
+)
   await app.listen(3333);
 }
 bootstrap();
