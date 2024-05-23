3f5e6620c3673b5914d2f13686ec5066e62de619
Safdar <safdar.hussain@devorbis.com>
2024-05-24 01:31:11 +0500

    change in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', ["module-resolver", {
+    "alias": {
+      "@": "./src"
+    }
+  }]],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
